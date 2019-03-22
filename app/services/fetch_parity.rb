a802cc42f93471bde83bbea00f6dd86f35d230b6
stephan-dev <stephan-dev@outlook.fr>
2019-03-22 08:55:50 +0100

    better dynamic endpoints

--- app/services/fetch_parity.rb
+++ app/services/fetch_parity.rb
@@ -1,4 +1,7 @@
 class FetchParity
+  require "erb"
+  include ERB::Util
+
   def example
     url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=bilan-social-effectifs-non-titulaires-permanents&facet=annee&facet=collectivite&facet=type_de_contrat&facet=emplois&facet=niveau&refine.emplois=TECHNICIENS%20SUPERIEURS%20D%27ADMINISTRATIONS%20PARISIENNES'
     response = HTTParty.get(url)
@@ -8,16 +11,20 @@ class FetchParity
     puts parsed["records"][0]["fields"]["nombre_d_hommes"]
   end
 
+  def construct_url(employee)
+    # employee = Employee.first
 
-  URI.escape("Techniciens supérieurs d'administrations parisiennes", "', ")
+    if employee.try(:job_level) == "CHARGE DE MISSION AGENT D'EXECUTION"
+      employee.job_level = "AGENT D'EXECUTION"
+    end
 
-  def construct_url
-    employee = Employee.first
-
-    if employee.job_specialty != nil
-      url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=bilan-social-effectifs-non-titulaires-permanents&facet=annee&refine.annee='+employee.year.to_s+'&facet='+employee.collectivity+'&facet=type_de_contrat&refine='+URI.escape(employee.contract_type, "', ")+'&facet=emplois&refine='+URI.escape(employee.job_title, "', ")+'&facet=niveau&refine.niveau='+URI.escape(employee.job_level, "', ")+'&refine.emplois='+URI.escape(employee.job_specialty, "', ")
+    if employee.try(:job_specialty)
+      # déplacé collectivité plus haut, et remplacé facet par refine (ayant eu "commune n'est pas une facet") l'ayant vu comme supercatégorie dans un exemple de retour d'api...
+      # copié la même plus bas
+      # essayé de préciser tous les refine.qqch
+      url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=bilan-social-effectifs-non-titulaires-permanents&refine='+employee.collectivity+'facet=annee&refine.annee='+employee.year.to_s+'&facet=type_de_contrat&refine.type_de_contrat='+url_encode(employee.contract_type)+'&facet=emplois&refine.emplois='+url_encode(employee.job_title)+'&facet=niveau&refine.niveau='+url_encode(employee.job_level)+'&refine.emplois='+url_encode(employee.job_specialty)
     else
-      url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=bilan-social-effectifs-non-titulaires-permanents&facet=annee&refine.annee='+employee.year.to_s+'&facet='+employee.collectivity+'&facet=type_de_contrat&refine='+URI.escape(employee.contract_type, "', ")+'&facet=emplois&refine='+URI.escape(employee.job_title, "', ")+'&facet=niveau&refine.niveau='+URI.escape(employee.job_level, "', ")
+      url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=bilan-social-effectifs-non-titulaires-permanents&refine='+employee.collectivity+'facet=annee&refine.annee='+employee.year.to_s+'&facet='+'&facet=type_de_contrat&refine.type_de_contrat='+url_encode(employee.contract_type)+'&facet=emplois&refine.emplois='+url_encode(employee.job_title)+'&facet=niveau&refine.niveau='+url_encode(employee.job_level)
     end
   end
 

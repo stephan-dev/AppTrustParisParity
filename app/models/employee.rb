36399df8b5576630ec517a240df899c1bc4c4081
stephan-dev <stephan-dev@outlook.fr>
2019-03-22 04:52:34 +0100

    duplicate employee replaces old one

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,6 @@
 class Employee < ApplicationRecord
-  # Identical row can't be persisted several times in several imports
-  validates :job_level, uniqueness: { scope: [:year, :collectivity, :contract_type, :job_title, :job_specialty] }, :message => "doublon"
+  # Identical rows can't be persisted
+  validates :job_level, uniqueness: { scope: [:year, :collectivity, :contract_type, :job_title, :job_specialty] }
 
 
   def self.import(file)
@@ -9,7 +9,14 @@ class Employee < ApplicationRecord
       # skips headers because they're in french and they have accents
       next if i == 0
 
-      Employee.create!(year: row[0], collectivity: row[1], contract_type: row[2], job_title: row[3], job_level: row[4], job_specialty: row[5])
+      employee = Employee.new(year: row[0], collectivity: row[1], contract_type: row[2], job_title: row[3], job_level: row[4], job_specialty: row[5])
+
+      begin
+        employee.save!
+      rescue # if validation fails
+        # the newest identical row is the most relevant
+        Employee.where(year: row[0], collectivity: row[1], contract_type: row[2], job_title: row[3], job_level: row[4], job_specialty: row[5]).update(year: row[0], collectivity: row[1], contract_type: row[2], job_title: row[3], job_level: row[4], job_specialty: row[5], updated_at: Time.now)
+      end
 
     end
   end

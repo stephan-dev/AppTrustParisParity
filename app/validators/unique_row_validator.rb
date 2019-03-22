cb2fedc13b3ae7d22d168713b40817937ae712d2
stephan-dev <stephan-dev@outlook.fr>
2019-03-22 03:42:32 +0100

    remove bogus uniqueness validator

--- app/validators/unique_row_validator.rb
+++
@@ -1,7 +0,0 @@
-class UniqueRowValidator < ActiveModel::Validator
-  def validate(record)
-    if record.attributes_for_uniqueness.values.uniq.size == 1
-      record.errors[:base] << 'Il ne peut pas pas y avoir deux entrées (lignes) identiques'
-    end
-  end
-end

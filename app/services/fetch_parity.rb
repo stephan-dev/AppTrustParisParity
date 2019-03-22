3b21b0b5e0aef713b29f79e734017a29c0447db6
stephan-dev <stephan-dev@outlook.fr>
2019-03-22 05:37:29 +0100

    parse example response

--- app/services/fetch_parity.rb
+++ app/services/fetch_parity.rb
@@ -1,1 +1,8 @@
-fetch_parity
+class FetchParity
+  url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=bilan-social-effectifs-non-titulaires-permanents&facet=annee&facet=collectivite&facet=type_de_contrat&facet=emplois&facet=niveau&refine.emplois=TECHNICIENS%20SUPERIEURS%20D%27ADMINISTRATIONS%20PARISIENNES'
+  response = HTTParty.get(url)
+
+  parsed = JSON.parse(response.to_s)
+
+  puts parsed["records"][0]["fields"]["nombre_d_hommes"]
+end

35bb21a13a146c68406cc3619cb7b1295359610b
stephan-dev <stephan-dev@outlook.fr>
2019-03-24 22:44:56 +0100

    fix endpoint query encoding

--- app/services/fetch_parity.rb
+++ app/services/fetch_parity.rb
@@ -18,21 +18,12 @@ class FetchParity
       employee.job_level = "AGENT D'EXECUTION"
     end
 
-    if employee.try(:job_specialty)
-      # déplacé collectivité plus haut, et remplacé facet par refine (ayant eu "commune n'est pas une facet") l'ayant vu comme supercatégorie dans un exemple de retour d'api...
-      # copié la même plus bas
-      # essayé de préciser tous les refine.qqch
-      url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=bilan-social-effectifs-non-titulaires-permanents&refine='+employee.collectivity+'facet=annee&refine.annee='+employee.year.to_s+'&facet=type_de_contrat&refine.type_de_contrat='+url_encode(employee.contract_type)+'&facet=emplois&refine.emplois='+url_encode(employee.job_title)+'&facet=niveau&refine.niveau='+url_encode(employee.job_level)+'&refine.emplois='+url_encode(employee.job_specialty)
+
+    if employee.try!(:job_specialty)
+      url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=bilan-social-effectifs-non-titulaires-permanents&facet=annee&refine.annee='+employee.year.to_s+'&facet=collectivite&refine.collectivite='+employee.collectivity+'&facet=type_de_contrat&refine.type_de_contrat='+url_encode(employee.contract_type)+'&facet=emplois&refine.emplois='+url_encode(employee.job_title)+'&facet=niveau&refine.niveau='+url_encode(employee.job_level)+'&refine.emplois='+url_encode(employee.job_specialty)
     else
-      url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=bilan-social-effectifs-non-titulaires-permanents&refine='+employee.collectivity+'facet=annee&refine.annee='+employee.year.to_s+'&facet='+'&facet=type_de_contrat&refine.type_de_contrat='+url_encode(employee.contract_type)+'&facet=emplois&refine.emplois='+url_encode(employee.job_title)+'&facet=niveau&refine.niveau='+url_encode(employee.job_level)
+      url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=bilan-social-effectifs-non-titulaires-permanents&facet=annee&refine.annee='+employee.year.to_s+'&facet=collectivite&refine.collectivite='+employee.collectivity+'&facet=type_de_contrat&refine.type_de_contrat='+url_encode(employee.contract_type)+'&facet=emplois&refine.emplois='+url_encode(employee.job_title)+'&facet=niveau&refine.niveau='+url_encode(employee.job_level)
     end
   end
 
 end
-
-
-# employees = Employee.all
-
-# employees.each do |employee|
-#   employee
-# end
